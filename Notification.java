300fa54194197a8b52ef66cabaa80343cb9bde3f
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:38:53 -0500

    add Notification.java

---
+++ Notification.java
@@ -0,0 +1,18 @@
+public class Notification {
+
+	private String notificationID;
+	private String type;
+	private String message;
+	private boolean isEnabled;
+
+	public void toggleNotification() {
+		// TODO - implement Notification.toggleNotification
+		throw new UnsupportedOperationException();
+	}
+
+	public void sendAlert() {
+		// TODO - implement Notification.sendAlert
+		throw new UnsupportedOperationException();
+	}
+
+}
