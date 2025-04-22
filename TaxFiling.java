cb9d6d44c4abf7d348dfb4d9f73137988a43d596
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:40:31 -0500

    add TaxFiling.java

---
+++ TaxFiling.java
@@ -0,0 +1,23 @@
+public class TaxFiling {
+
+	private String taxFilingID;
+	private int year;
+	private String status;
+	private String reportLink;
+
+	public void linkTool() {
+		// TODO - implement TaxFiling.linkTool
+		throw new UnsupportedOperationException();
+	}
+
+	public void submitTaxes() {
+		// TODO - implement TaxFiling.submitTaxes
+		throw new UnsupportedOperationException();
+	}
+
+	public File downloadReport() {
+		// TODO - implement TaxFiling.downloadReport
+		throw new UnsupportedOperationException();
+	}
+
+}
