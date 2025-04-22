960ae154e086fff34eafbca8b35de9e7aee00fba
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:38:24 -0500

    add Log.java

---
+++ Log.java
@@ -0,0 +1,22 @@
+public class Log {
+
+	private String logID;
+	private Timestamp timestamp;
+	private String action;
+
+	public void recordAction() {
+		// TODO - implement Log.recordAction
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Log> getUserLogs() {
+		// TODO - implement Log.getUserLogs
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Log> getLogsByDate() {
+		// TODO - implement Log.getLogsByDate
+		throw new UnsupportedOperationException();
+	}
+
+}
