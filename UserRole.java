b1745a7b23c2aef42b20e07a3b48f3526946de03
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:41:14 -0500

    add UserRole.java

---
+++ UserRole.java
@@ -0,0 +1,15 @@
+public class UserRole {
+
+	private String roleID;
+	private String roleName;
+
+	public void assignRoleToUser() {
+		// TODO - implement UserRole.assignRoleToUser
+		throw new UnsupportedOperationException();
+	}
+
+	public String getRoleName() {
+		return this.roleName;
+	}
+
+}
