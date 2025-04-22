bfc07753c8512fcd174d48e0d0707d016423a649
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:37:30 -0500

    add FinancialRepReport.java

---
+++ FinancialRepReport.java
@@ -0,0 +1,22 @@
+public class FinancialRepReport {
+
+	private String reportID;
+	private String title;
+	private Date createdDate;
+
+	public File generateReport() {
+		// TODO - implement FinancialRepReport.generateReport
+		throw new UnsupportedOperationException();
+	}
+
+	public File downloadReport() {
+		// TODO - implement FinancialRepReport.downloadReport
+		throw new UnsupportedOperationException();
+	}
+
+	public String viewReportTitle() {
+		// TODO - implement FinancialRepReport.viewReportTitle
+		throw new UnsupportedOperationException();
+	}
+
+}
