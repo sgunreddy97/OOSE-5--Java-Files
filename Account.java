8d6774657b22172aa4b4c09e7137583a18c0195b
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:35:23 -0500

    Add Account.java

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account {
+
+	private String accountNumber;
+	private String deliveryFrequency;
+	private String accountType;
+
+	public String viewAccountDetails() {
+		// TODO - implement Account.viewAccountDetails
+		throw new UnsupportedOperationException();
+	}
+
+}
