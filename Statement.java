9d5e626a91022737fc6a6aa45a2fb15606d81592
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:39:16 -0500

    add Statement.java

---
+++ Statement.java
@@ -0,0 +1,23 @@
+public class Statement {
+
+	private String statementID;
+	private Date statementDate;
+	private String type;
+	private String fileLink;
+
+	public File download() {
+		// TODO - implement Statement.download
+		throw new UnsupportedOperationException();
+	}
+
+	public void filterByDate() {
+		// TODO - implement Statement.filterByDate
+		throw new UnsupportedOperationException();
+	}
+
+	public void filterByType() {
+		// TODO - implement Statement.filterByType
+		throw new UnsupportedOperationException();
+	}
+
+}
