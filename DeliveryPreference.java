02644c689f6e922ed1d2d635c9785aa57fa09df9
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:36:53 -0500

    Add DeliveryPreference.java

---
+++ DeliveryPreference.java
@@ -0,0 +1,16 @@
+public class DeliveryPreference {
+
+	private String preferenceID;
+	private String preferenceType;
+
+	public String updatePreference() {
+		// TODO - implement DeliveryPreference.updatePreference
+		throw new UnsupportedOperationException();
+	}
+
+	public String showFeeWarning() {
+		// TODO - implement DeliveryPreference.showFeeWarning
+		throw new UnsupportedOperationException();
+	}
+
+}
