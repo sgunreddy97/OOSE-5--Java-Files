9d73e3d49e9393788d635009d45c88946f488719
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-22 18:40:54 -0500

    add User.java

---
+++ User.java
@@ -0,0 +1,25 @@
+public class User {
+
+	private String userID;
+	private String name;
+	private String email;
+	private String phone;
+	private String last4SSN;
+	private String password;
+
+	public boolean login() {
+		// TODO - implement User.login
+		throw new UnsupportedOperationException();
+	}
+
+	public String register() {
+		// TODO - implement User.register
+		throw new UnsupportedOperationException();
+	}
+
+	public void resetPassword() {
+		// TODO - implement User.resetPassword
+		throw new UnsupportedOperationException();
+	}
+
+}
