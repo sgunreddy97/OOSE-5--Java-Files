c11fe049fc4ea132802ed4e606427480cc41dd49
sgunreddy97 <s.gunreddy@eagles.oc.edu>
2025-04-28 21:16:40 -0500

    Create SupportRequestTest.java

---
+++ test/SupportRequestTest.java
@@ -0,0 +1,18 @@
+package financialapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupportRequestTest {
+    @Test
+    void testSupportRequestFields() {
+        SupportRequest request = new SupportRequest();
+        request.setSupportID("S001");
+        request.setIssueDescription("Login issue");
+        request.setDateSubmitted("2025-04-28");
+
+        assertEquals("S001", request.getSupportID());
+        assertEquals("Login issue", request.getIssueDescription());
+        assertEquals("2025-04-28", request.getDateSubmitted());
+    }
+}
